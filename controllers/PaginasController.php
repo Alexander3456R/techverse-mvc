05be9024243ed0ee905b55101d4a9eef405c548c
Christopher <morochito344@gmail.com>
2025-07-31 22:54:48 -0500

    verificando que este autenticado

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -1,7 +1,13 @@
 <?php
 
 namespace Controllers;
+
+use Model\Evento;
 use MVC\Router;
+use Model\Categoria;
+use Model\Dia;
+use Model\Hora;
+use Model\Expositor;
 
 class PaginasController {
 
@@ -12,6 +18,7 @@ class PaginasController {
     }
 
     public static function evento(Router $router) {
+
         $router->render('paginas/devwebcamp', [
             'titulo' => 'Sobre TechVerse'
         ]);
@@ -26,8 +33,36 @@ class PaginasController {
 
 
     public static function conferencias(Router $router) {
+
+        $eventos = Evento::ordenar('hora_id', 'ASC');
+
+        $eventos_formateados = [];
+        foreach($eventos as $evento) {
+            $evento->categoria = Categoria::find($evento->categoria_id);
+            $evento->dia = Dia::find($evento->dia_id);
+            $evento->hora = Hora::find($evento->hora_id);
+            $evento->expositor = Expositor::find($evento->expositor_id);
+
+            if($evento->dia_id === '1' && $evento->categoria_id === '1') {
+                $eventos_formateados['conferencias_j'][] = $evento;
+            }
+
+            if($evento->dia_id === '2' && $evento->categoria_id === '1') {
+                $eventos_formateados['conferencias_v'][] = $evento;
+            }
+
+
+            if($evento->dia_id === '1' && $evento->categoria_id === '2') {
+                $eventos_formateados['workshops_j'][] = $evento;
+            }
+
+            if($evento->dia_id === '2' && $evento->categoria_id === '2') {
+                $eventos_formateados['workshops_v'][] = $evento;
+            }
+        }
         $router->render('paginas/conferencias', [
-            'titulo' => 'Conferencias & Workshops'
+            'titulo' => 'Conferencias & Workshops',
+            'eventos' => $eventos_formateados
         ]);
     }
 
---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+import './horas.js';
+import './expositores.js';
+import './tags.js';
+import './slider.js';
